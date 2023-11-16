d86325557ad08028cb0b58c21a4b8eac449aa40e
Ethan Evans <ethanalexevans@gmail.com>
2023-11-16 11:38:16 -0800

    Initial

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using OpenCvSharp;
+
+if (args.Length < 1) {
+    Console.WriteLine("Usage: quickImgViewer <image>");
+    return;
+}
+
+var imagePath = args[0];
+
+try {
+    if (!File.Exists(imagePath)) {
+        Console.WriteLine($"File not found: {imagePath}");
+        return;
+    }
+} catch (Exception e) {
+    Console.Error.WriteLine(e.Message);
+    return;
+} 
+
+using var src = new Mat(
+    imagePath,
+    ImreadModes.Color
+);
+
+try {
+    using var window = new Window(
+        "image",
+        src,
+        WindowFlags.Normal
+        | WindowFlags.KeepRatio
+    );
+
+    window.Move(0, 0);
+    window.Resize(src.Width, src.Height);
+
+    Cv2.WaitKey();
+    window.Close();
+} catch (OpenCVException e) {
+    Console.Error.WriteLine(e.Message);
+}
+
